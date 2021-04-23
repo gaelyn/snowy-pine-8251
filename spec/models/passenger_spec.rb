f864bf16f19de96b3c103fe9dc3be3d885b83856
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-04-23 11:23:06 -0500

    create model and feature tests for extension

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,18 +5,4 @@ RSpec.describe Passenger, type: :model do
     it { should have_many(:tickets) }
     it { should have_many(:flights).through(:tickets) }
   end
-
-  # before :each do
-  #
-  # end
-  #
-  # describe 'class methods' do
-  #   describe '#' do
-  #   end
-  # end
-  #
-  # describe 'instance methods' do
-  #   describe '.' do
-  #   end
-  # end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -5,18 +5,4 @@ RSpec.describe Ticket, type: :model do
     it { should belong_to(:flight) }
     it { should belong_to(:passenger) }
   end
-
-  # before :each do
-  #
-  # end
-  #
-  # describe 'class methods' do
-  #   describe '#' do
-  #   end
-  # end
-  #
-  # describe 'instance methods' do
-  #   describe '.' do
-  #   end
-  # end
 end
